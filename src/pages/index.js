a606d27c6869cf5d90ce7c98d19c4c38c2ffb221
mackil5000 <41338724+mackil5000@users.noreply.github.com>
2018-12-22 16:19:33 +0100

    hej

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,48 +2,109 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
 export default class IndexPage extends React.Component {
   render() {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
-
     return (
       <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+      <div 
+      className="container-fluid"
+      style={{
+        marginTop: '80px'
+      }}
+      >
+        <div className="row">
+
+          <div
+            className="col-lg-3 d-none d-lg-block"
+            style={{
+              position: 'fixed',
+              left: '0'
+             }}
+          >
+            <div className="row"
+            >
+              <div className="col-12">
+                <h1 className="text-white">Senaste inläggen</h1>
+              </div>
+              {posts
+                .map(({ node: post }) => (
+                  <div
+                    className="col-12 post-list"
+                    id={post.fields.slug}
+                    key={post.id}
+                  >
+
+                    <ul>
+                      <li>
+                        <Link 
+                        className="has-text-primary" 
+                        id="link"
+                        to={post.fields.slug}>
+                        {post.frontmatter.title}
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                ))}
             </div>
-            {posts
-              .map(({ node: post }) => (
-                <div
-                  className="content"
-                  style={{ border: '1px solid #333', padding: '2em 4em' }}
-                  key={post.id}
-                >
-                  <p>
-                    <Link className="has-text-primary" to={post.fields.slug}>
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <small>{post.frontmatter.date}</small>
-                  </p>
-                  <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
-                    </Link>
-                  </p>
-                </div>
-              ))}
           </div>
-        </section>
-      </Layout>
+          
+  
+
+	
+          <div
+            className="col-lg-9"
+            style={{
+              position: 'absolute',
+              right: '0'
+            }}
+          >
+            <div
+              className="card-columns" // row
+            >
+
+              {posts
+                .map(({ node: post }) => (
+
+                  <div
+                    className="card post-tile" //col-x-x
+                    key={post.id}
+                  >
+                    <Link to={post.fields.slug}>
+                      <img
+                        className="img-fluid"
+                        style={{ borderRadius: '5px' }}
+                        src={post.frontmatter.image}
+                      />
+                      <PreviewCompatibleImage imageInfo={post.frontmatter.image} />
+                    </Link>
+                    <h4 className="card-heading">
+                      <Link className="" to={post.fields.slug}>
+                        {post.frontmatter.title}
+                      </Link>
+                      {/* <small>{post.frontmatter.date}</small> */}
+                    </h4>
+                    <p className="d-none d-md-block">
+                      {/* {post.excerpt} */}
+                      {/* <br />
+                        <br /> */}
+                      {/* <Link className="" to={post.fields.slug}> */}
+                      {/* </Link> */}
+                    </p>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+      </div>
+</Layout>
     )
   }
+  
 }
 
 IndexPage.propTypes = {
