9d3c523137f96002cfa19f02f48603b3a4b21cb1
mackil5000 <41338724+mackil5000@users.noreply.github.com>
2018-12-22 17:14:55 +0100

    maggan

--- src/pages/index.js
+++ src/pages/index.js
@@ -130,6 +130,7 @@ export const pageQuery = graphql`
                     ...GatsbyImageSharpFluid
                   }
                 }
+                id
               }
             }
             templateKey
